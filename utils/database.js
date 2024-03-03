06a184678bb5a25bc37ee532e1b37d10811dcf34
Matias Tellini <tellini.matias@gmail.com>
2024-03-03 14:22:21 -0300

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -4,7 +4,7 @@ let isConnected = false; // track the connection
 
 export const connectToDB = async () => {
   mongoose.set('strictQuery', true);
-  const uri ='mongodb+srv://tellinimatias:mxcjLTAp7l1iun2b@cluster0.hqxxnx1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
 
   if(isConnected) {
     console.log('MongoDB is already connected');
@@ -12,7 +12,7 @@ export const connectToDB = async () => {
   }
 
   try {
-    await mongoose.connect(uri, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "share_prompt",
       useNewUrlParser: true,
       useUnifiedTopology: true,
