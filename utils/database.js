7e8f0b8a3475e7d8ea2a3f3b197a5414a7a39b94
Matias Tellini <tellini.matias@gmail.com>
2024-03-03 18:16:25 -0300

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -5,7 +5,6 @@ let isConnected = false; // track the connection
 export const connectToDB = async () => {
   mongoose.set('strictQuery', true);
 
-
   if(isConnected) {
     console.log('MongoDB is already connected');
     return;
